df264f2ab9b58a8c500caca323c39bd6a8b75974
vamshinfo <vamshinfomile@gmail.com>
2018-10-09 18:48:17 +0530

    add

--- src/AppBundle/Controller/HerokuController.php
+++ src/AppBundle/Controller/HerokuController.php
@@ -5,42 +5,11 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Symfony\Component\HttpFoundation\Request;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use AppBundle\contact\Contact;
-use AppBundle\Form\ContactType;
-
 
 class HerokuController extends Controller
-// {
-// 	/**
-//      * 
-//      * @Route("/my/new", name="heroku")
-//      * @Method("POST")
-//      * @Template("AppBundle:Contact:new.html.twig")
-//      */
-//     public function indexAction()
-//     {
-//         $contact = new Contact();
-//         $form = $this->createCreateForm($contact);
-//         $form->handleRequest($request);
-
-//         if ($form->isValid()) {
-//             $em = $this->getDoctrine()->getManager();
-//             $em->persist($contact);
-//             $em->flush();
-
-//             return $this->redirect($this->generateUrl('myaction', array('id' => $contact->getId())));
-//         }
-
-//         return array(
-//             'contact' => $Contact,
-//             'form'   => $form->createView(),
-//         );
-//     }
-
+{
     /**
-     * @Route("/my" name="myaction")
+     * @Route("/my")
      * @Template()
      */
     public function myAction()
@@ -52,56 +21,40 @@ class HerokuController extends Controller
         $statement = $em->getConnection()->prepare($RAW_QUERY);
         $statement->execute();
         $result = $statement->fetchAll();
-        // var_dump($result);
+        var_dump($result);
     	    } 
         catch (\Doctrine\ORM\NoResultException $e) {
              return null;
             }
 
-          return array('result' => $result);
+          return $this->render('AppBundle:Page:my.html.twig', array('result' => $result));
 
     }
 
-    
-
-
-
     // /**
-    //  * Creates a form to create a Contact contact.
-    //  *
-    //  * @param Contact $contact The contact
-    //  *
-    //  * @return \Symfony\Component\Form\Form The form
-    //  */
-    // private function createCreateForm(Contact $contact)
-    // {
-    //     $form = $this->createForm(new ContactType(), $contact, array(
-    //         'action' => $this->generateUrl('Contact_create'),
-    //         'method' => 'POST',
-    //     ));
-
-    //     $form->add('submit', 'submit', array('label' => 'Create'));
-
-    //     return $form;
-    // }
-
-    // /**
-    //  * Displays a form to create a new Contact contact.
-    //  *
-    //  * @Route("/new", name="Contact_new")
+    //  * 
+    //  * @Route("/", name="heroku")
     //  * @Method("GET")
     //  * @Template()
     //  */
-    // public function newAction()
+    // public function indexAction()
     // {
-    //     $contact = new Contact();
-    //     $form   = $this->createCreateForm($contact);
+    //     $contact = new Person();
+    //     $form = $this->createCreateForm($conta);
+    //     $form->handleRequest($request);
+
+    //     if ($form->isValid()) {
+    //         $em = $this->getDoctrine()->getManager();
+    //         $em->persist($entity);
+    //         $em->flush();
+
+    //         return $this->redirect($this->generateUrl('person_show', array('id' => $entity->getId())));
+    //     }
 
     //     return array(
-    //         'contact' => $contact,
+    //         'entity' => $entity,
     //         'form'   => $form->createView(),
     //     );
     // }
-
 
 }
